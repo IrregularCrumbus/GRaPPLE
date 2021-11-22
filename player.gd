00f78f57e077536d33974a6872a2884ec0afee1c
Doucette <sadouc01@012-imac-140-27.local>
2021-11-22 08:23:58 -0500

    Basically completed 3d platformer base; still have a bug to fix

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ const gravity = 0.98
 const maxFallSpeed = 30
 
 const xLookSensitivity = 1.0
-const yLookSensitivity = 1.0\
+const yLookSensitivity = 1.0
 
 var yVelocity = 0
 
@@ -30,12 +30,35 @@ func _input(event):
 		rotation_degrees.y -= event.relative.x * xLookSensitivity
 
 func _physics_process(delta):
-	var moveVector = Vector3()
+	# Enabling the player to move with WASD
+	var moveVec = Vector3()
 	if Input.is_action_pressed("moveForwards"):
-		moveVector.z -=1
+		moveVec.z -=1
 	if Input.is_action_pressed("moveBackwards"):
-		moveVector.z +=1
+		moveVec.z +=1
 	if Input.is_action_pressed("moveRight"):
-		moveVector.x +=1
+		moveVec.x +=1
 	if Input.is_action_pressed("moveLeft"):
-		moveVector.x -=1
+		moveVec.x -=1
+	moveVec = moveVec.normalized()
+	moveVec = moveVec.rotated(Vector3(0, 1, 0), rotation.y)
+	moveVec *= moveSpeed
+	moveVec = yVelocity
+	move_and_slide(moveVec, Vector3(0, 1, 0))
+	
+	# Handles physics in the Y axis: causes gravity to exist, allows the player to jump, and sets a cap to how fast the player can fall
+	var grounded = is_on_floor()
+	yVelocity -= gravity
+	var justJumped = false
+	if grounded and Input.is_action_just_pressed("jump"):
+		justJumped = true
+		yVelocity = jumpForce
+	if grounded and yVelocity <= 0:
+		yVelocity = -0.1
+	if yVelocity < -maxFallSpeed:
+		yVelocity = -maxFallSpeed
+		
+func playAnimations(name):
+	# Again, I may or may not even use this funcion if I get animations, since this is a first person platformer.
+	pass
+
